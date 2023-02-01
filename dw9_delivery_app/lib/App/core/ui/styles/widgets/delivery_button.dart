192e5fd2e7c33745ddcadeb7670966ea6b9c51f9
murilo <muriloamancio0@gmail.com>
2023-01-31 23:37:48 -0300

    Button_config

---
+++ dw9_delivery_app/lib/App/core/ui/styles/widgets/delivery_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DeliveryButton extends StatelessWidget {
+
+    final String label;
+    //referencia de uma funcao que retorna void
+    final VoidCallback? onPressed; 
+    final double? width;
+    final double? height;
+
+  const DeliveryButton({ 
+    super.key,
+    required this.label, 
+    required this.onPressed,
+    this.height = 50,
+    this.width,
+    });
+
+//elevated button é definido o tamanho pelo pai, geralmente envolto de umn SizeBox
+   @override
+   Widget build(BuildContext context) {
+       return SizedBox(
+        width: width,
+        height: height,
+        child: ElevatedButton(
+          onPressed: (){},
+          child: Text(label)
+        ),
+       );
+  }
+}
--- dw9_delivery_app/lib/App/pages/splash/splash_page.dart
+++ dw9_delivery_app/lib/App/pages/splash/splash_page.dart
@@ -2,6 +2,7 @@ import 'package:dw9_delivery_app/App/core/ui/styles/colors_app.dart';
 import 'package:flutter/material.dart';
 
 import '../../core/ui/styles/app_styles.dart';
+import '../../core/ui/styles/widgets/delivery_button.dart';
 
 class SplashPage extends StatelessWidget {
 
@@ -18,9 +19,14 @@ class SplashPage extends StatelessWidget {
           body: Column(
             children: [
               Container(),
-              ElevatedButton(onPressed: (){}, child: Text('Teste')),
+              DeliveryButton(
+                width: 200,
+                height: 50,
+                label: 'Teste label',
+                onPressed: (){},
+              ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'text'),
+                decoration: const InputDecoration(labelText: 'text'),
               )
             ],
           ),
